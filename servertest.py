4823f7854ce54d1927d9c1a8c741d655210a3c76
jk2pro <dbwhdrnr3@gamil.com>
2023-06-01 13:45:19 +0900

    "test"

---
+++ servertest.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home() :
+    return 'Hello, World'
+
+if __name__ == '__main__':
+    app.run()
+
+
+# !pip install cx_Oracle --upgrade
+# !pip install oracledb --upgrade
+import cx_Oracle as cx
+conn = cx.connect("campus_c_230531_3","smhrd3","project-db-stu.smhrd.com:1524/XE",encoding="UTF-8")
+    
